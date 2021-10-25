6a46405f81165ad64122681972d3e39565fe3ee8
Yadurani Lopez Rivera <yadu-lopez@hotmail.com>
2021-10-25 10:12:11 -0500

    Fix: Update variables

--- src/sanityClient.js
+++ src/sanityClient.js
@@ -1,6 +1,6 @@
 import sanityClient from '@sanity/client'
 
 export default sanityClient({
-    projectId: "djtlwm1o",
-    dataset: "production"
+    projectId: "IDPROJECT",
+    dataset: "DATASET"
 })
